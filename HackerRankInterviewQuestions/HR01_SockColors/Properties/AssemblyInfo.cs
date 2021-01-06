4ad40d21de1ed4c27d7e0377214275f3243cc4b1
Prescott Hodge <smallaxe@gmail.com>
2021-01-05 23:35:05 -0800

    F!! ph Added test project to complete first question

---
+++ HackerRankInterviewQuestions/HR01_SockColors/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HR01_SockColors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HR01_SockColors")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e668346-f0d2-40bb-a179-66506f194072")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HackerRankInterviewQuestions/HR01_SockColors/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HR01_SockColors
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NineSocksWith3Matches()
+        {
+            int n = 9;
+            int[] ar = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            Socks newSocks = new Socks();
+
+            int numberOfPairs = Socks.SockMerchant(n, ar);
+
+            Assert.AreEqual(3, numberOfPairs);
+        }
+    }
+
+    public class Socks
+    {
+        public static int SockMerchant(int n, int[] ar)
+        {
+            List<int> sockCollection = new List<int>();
+            int numberSockPairs = 0;
+            
+            //loop through array from 0 --> n and read each color in ar
+            for (int i = 0; i < n; i++)
+            {
+                //check if current color in collection
+                if (!sockCollection.Contains(ar[i]))
+                {
+                    //save sock colors in an array 
+                    sockCollection.Add(ar[i]);
+                }
+                else
+                {
+                    sockCollection.Remove(ar[i]);
+                    numberSockPairs++;
+                }
+            }
+
+            return numberSockPairs;
+        }
+    }
+}
+
+
+
+
+
+
+    
+
+
+
+
